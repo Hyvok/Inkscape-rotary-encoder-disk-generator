66cb9a7bdb92ab3458f8d336bd472c56c5f09403
Kalle Hyvönen <kalle.hyvonen@dgkelectronics.com>
2012-07-10 11:35:33 +0300

    Code refactored to look prettier

--- encoder_disk_generator.py
+++ encoder_disk_generator.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import inkex, gettext
+import inkex
 import cubicsuperpath, simplestyle, math
-_ = gettext.gettext
 
-# Function for calculating a point when you know the distance and the angle
-# from the origin
+# Function for calculating a point from the origin when you know the distance 
+# and the angle
 def calculatePoint(angle, distance):
-	if angle < 0.0 or angle > 360.0:
+	if angle < 0 or angle > 360:
 		return None
-	elif angle == 0.0 or angle == 360.0:
-		return [distance, 0.0]
-	elif angle == 90.0:
-		return [0.0, distance]
-	elif angle == 180.0:
-		return [-1.0*distance, 0.0]
-	elif angle == 270.0:
-		return [0.0, -1.0*distance]
 	else:
-		return [distance*math.cos(math.radians(angle)), distance*math.sin(math.radians(angle))]
+		return [distance*math.cos(math.radians(angle)),
+				distance*math.sin(math.radians(angle))]
 
 class EncoderDiskGenerator(inkex.Effect):
 		
@@ -54,81 +46,99 @@ class EncoderDiskGenerator(inkex.Effect):
 				        dest="inner_encoder_width", default=0.0,
 				        help="Width of the inner encoder disk")
 
+	def parsePathData(self, command, point):
+		path_data = command + ' '
+
+		path_data += str(point[0]) + ' ' + str(point[1]) + ' '
+		return path_data
+
 	def drawSegment(self, line_style, angle, segment_angle, outer_diameter, width):
 
-		line_attributes = {'style' : simplestyle.formatStyle(line_style)}
+		path = {'style' : simplestyle.formatStyle(line_style)}
+		path['d'] = ''
+		outer_radius = outer_diameter/2
 
 		# Go to the first point in the segment
-		point = calculatePoint(angle, (outer_diameter-width)/2.0)
-		line_attributes['d'] = 'M '+str(point[0])+' '+str(point[1])
+		point = calculatePoint(angle, outer_radius-width)
+		path['d'] += self.parsePathData('M', point)
 
 		# Go to the second point in the segment
-		point = calculatePoint(angle, outer_diameter/2.0)
-		line_attributes['d'] += ' L '+ str(point[0])+' '+str(point[1])
+		point = calculatePoint(angle, outer_radius)
+		path['d'] += self.parsePathData('L', point)
 
 		# Go to the third point in the segment, draw an arc
-		point = calculatePoint(angle+segment_angle, outer_diameter/2.0)
-		control_point = calculatePoint(angle+(segment_angle/2.0), (outer_diameter/2.0)/(math.cos(math.radians(segment_angle/2.0))))
-		line_attributes['d'] += ' Q '+ str(control_point[0]) + ',' + str(control_point[1]) + ' ' + str(point[0])+','+str(point[1])
+		point = calculatePoint(angle+segment_angle, outer_radius)
+		control_point_distance_to_origin = outer_radius/(math.cos(math.radians(segment_angle/2)))
+		control_point = calculatePoint(angle+(segment_angle/2), control_point_distance_to_origin)
+		path['d'] += self.parsePathData('Q', control_point) + self.parsePathData(' ', point)
 
 		# Go to the fourth point in the segment
-		point = calculatePoint(angle+segment_angle,(outer_diameter-width)/2.0)
-		line_attributes['d'] += ' L '+ str(point[0])+' '+str(point[1])
+		point = calculatePoint(angle+segment_angle, outer_radius-width)
+		path['d'] += self.parsePathData('L', point)
 
 		# Go to the beginning in the segment, draw an arc
-		point = calculatePoint(angle, (outer_diameter-width)/2.0)
-		control_point = calculatePoint(angle+(segment_angle/2.0), ((outer_diameter-width)/2.0)/(math.cos(math.radians(segment_angle/2.0))))
-		line_attributes['d'] += ' Q '+ str(control_point[0]) + ',' + str(control_point[1]) + ' ' + str(point[0])+','+str(point[1])
-
-
+		point = calculatePoint(angle, outer_radius-width)
+		control_point_distance_to_origin = (outer_radius-width)/(math.cos(math.radians(segment_angle/2)))
+		control_point = calculatePoint(angle+(segment_angle/2), control_point_distance_to_origin)
 		# 'Z' closes the path
-		line_attributes['d'] += ' Z'
+		path['d'] += self.parsePathData('Q', point) + self.parsePathData(' ', point) + ' Z'
 
-		#inkex.errormsg(_("Data = " + str(line_attributes['d'])))
+		# Return the path
+		return path
 
-		# Add the path to the document
-		return line_attributes
+	def addElement(self, element_type, group, element_attributes):
+		self.current_layer.append(inkex.etree.SubElement(group, 
+		inkex.addNS(element_type,'svg'), element_attributes))
 
 	def effect(self):
 
 		# Group to put all the elements in, center set in the middle of the view
-		group = inkex.etree.SubElement(self.current_layer, 'g', {inkex.addNS('label', 'inkscape'):'Encoder disk', 'transform':'translate' + str(self.view_center)})
+		group = inkex.etree.SubElement(self.current_layer, 'g', {
+			inkex.addNS('label', 'inkscape'):'Encoder disk', 
+			'transform':'translate' + str(self.view_center)
+		})
 
 		# Attributes for the center hole, then create it
 		attributes = {
-			'style'     : simplestyle.formatStyle({'stroke':'none', 'fill':'#000000'}),
-			'r'         : str(self.options.hole_diameter/2.0)
+			'style'     : simplestyle.formatStyle({'stroke':'none', 'fill':'black'}),
+			'r'         : str(self.options.hole_diameter/2)
 		}
-		self.current_layer.append(inkex.etree.SubElement(group, inkex.addNS('circle','svg'), attributes))
+		self.addElement('circle', group, attributes)
 
 		# Attributes for the guide hole in the center hole, then create it
 		attributes = {
 			'style'     : simplestyle.formatStyle({'stroke':'white','fill':'white'}),
 			'r'         : '1'
 		}
-		self.current_layer.append(inkex.etree.SubElement(group, inkex.addNS('circle','svg'), attributes))
+		self.addElement('circle', group, attributes)
 
 		# Attributes for the outer rim, then create it
 		attributes = {
 			'style'     : simplestyle.formatStyle({'stroke':'black', 'stroke-width':'1', 'fill':'none'}),
-			'r'         : str(self.options.diameter/2.0)
+			'r'         : str(self.options.diameter/2)
 		}
-		self.current_layer.append(inkex.etree.SubElement(group, inkex.addNS('circle','svg'), attributes))
+		self.addElement('circle', group, attributes)
 
 		# Line style for the encoder segments
-		line_style   = { 'stroke':'black',
-				         'stroke-width':'1',
-				         'fill':'black'
-				       }
+		line_style   = { 
+			'stroke'		: 	'black',
+			'stroke-width'	:	'1',
+			'fill'			:	'black'
+		}
 
-		for segment in range(0, self.options.segments, 2):
-			self.current_layer.append(inkex.etree.SubElement(group, inkex.addNS('path', 'svg'), self.drawSegment(line_style, segment*(360.0/self.options.segments), 360.0/self.options.segments, self.options.outer_encoder_diameter, self.options.outer_encoder_width)))
+		segment_angle = 360.0/self.options.segments
 
-		if self.options.inner_encoder_diameter > 0.0:
-			for segment in range(0, self.options.segments, 2):
-				self.current_layer.append(inkex.etree.SubElement(group, inkex.addNS('path', 'svg'), self.drawSegment(line_style, segment*(360.0/self.options.segments)+((360.0/self.options.segments)/2.0), 360.0/self.options.segments, self.options.inner_encoder_diameter, self.options.inner_encoder_width)))
+		for segment_number in range(0, self.options.segments, 2):
+			angle = segment_number*segment_angle
+			segment = self.drawSegment(line_style, angle, segment_angle,
+				self.options.outer_encoder_diameter, self.options.outer_encoder_width)
+			self.addElement('path', group, segment)
 
-
+			if self.options.inner_encoder_diameter > 0:
+				# The inner encoder must be half an encoder segment ahead of the outer one
+				segment = self.drawSegment(line_style, angle+segment_angle/2, segment_angle,
+					self.options.inner_encoder_diameter, self.options.inner_encoder_width)
+				self.addElement('path', group, segment)
 
 if __name__ == '__main__':
 	# Run the effect
